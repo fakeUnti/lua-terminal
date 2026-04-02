cb4f511f2dd9ec9af93295f83d496fc55c0fb63f
fakeUnti <Sviatoslav.khrushchov@gmail.com>
2026-04-02 08:22:01 -0400

    Update makedrfile.lua

--- makedrfile.lua
+++ makedrfile.lua
@@ -1,10 +1,16 @@
+function WAIT(seconds)
+    local start = os.time()
+    while os.time() - start + seconds do
+        --on purpose to have nothing inside do not worry
+    end
+end
 function MAKEDRFILE(useros,home)
     print("no directory file found!\n do you want to make one? [Y/N]")
     if io.read("*l"):sub(1,1):lower() == "y" then
         if useros == "windows" then
-           os.execute("type nul > startingdir.txt")--makes file windows
+           os.execute("type nul > startingdir.txt")--windows makes file
         else
-            local script_dir = arg[0]:match("(.*/)") or "./"
+            local script_dir = arg[0]:match("(.*/)") or "./" --i know this looks scary but its not dw it just detects where the file is run
             os.execute("touch " .. script_dir .. "startingdir.txt") --unix makes file
         end
         local file = io.open("startingdir.txt", "w") --opens
@@ -16,6 +22,7 @@ function MAKEDRFILE(useros,home)
         file:close() --closes
         print("file created!")
     else 
+        print("disclaimer: you will not be able to use the program if there is no directory file")
         return
     end
 end
